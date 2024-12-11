f46617e50d96e6991cd2739c6ae61395824dcb34
Scott Gillies <s_gillies123726@fanshaweonline.ca>
2024-12-11 13:11:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GUI_Creature_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
